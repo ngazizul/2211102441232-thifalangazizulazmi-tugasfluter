4afc02810aa9bfb68cd141c94c30f966b538094f
unknown <azmingazizul@gmail.com>
2024-03-20 23:00:01 +0800

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
